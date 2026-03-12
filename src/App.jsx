100b4b895d143e6c02d837daed63de1ed80cdce5
Vanessa Barsotti <vaplacencio@hotmail.com>
2026-03-12 13:50:08 -0400

    Testing App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get startedx</h1>
+          <h1>Dashboard</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>

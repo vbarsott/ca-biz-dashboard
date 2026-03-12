624c3c7481c910853c01c3f6c60b307645f61a6d
Vanessa Barsotti <vaplacencio@hotmail.com>
2026-03-12 13:29:59 -0400

    Set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/ca-biz-dashboard/", // 👈 repo name, with leading and trailing slash
+});

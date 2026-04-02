69d0238208e630adc6d330d0136cedec61fbdd53
Vanessa Barsotti <vaplacencio@hotmail.com>
2026-04-02 13:05:18 -0400

    Update slides according to new spreadsheet from April 1.

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,16 @@ import img2 from "./assets/slide-2.png";
 import img3 from "./assets/slide-3.png";
 import img4 from "./assets/slide-4.png";
 import img5 from "./assets/slide-5.png";
-import img6 from "./assets/slide-6.png";
 import "./App.css";
 
 function App() {
-  const images = [img1, img2, img3, img4, img5, img6];
+  const images = [img1, img2, img3, img4, img5];
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % images.length);
-    }, 3000); // seconds
+    }, 10000); // 10 seconds
 
     return () => clearInterval(interval);
   }, [images.length]);

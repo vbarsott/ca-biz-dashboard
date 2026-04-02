83f727b2d34201bb1942a95fe55d01d6a074df6a
Vanessa Barsotti <vaplacencio@hotmail.com>
2026-04-02 17:18:21 -0400

    Update slides with better titles and charts

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import img2 from "./assets/slide-2.png";
 import img3 from "./assets/slide-3.png";
 import img4 from "./assets/slide-4.png";
 import img5 from "./assets/slide-5.png";
+import img6 from "./assets/slide-6.png";
+import img7 from "./assets/slide-7.png";
+import img8 from "./assets/slide-8.png";
 import "./App.css";
 
 function App() {
-  const images = [img1, img2, img3, img4, img5];
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8];
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
